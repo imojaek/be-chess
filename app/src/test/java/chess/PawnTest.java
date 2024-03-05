c669bc43d57046039c3d1ead199591358d5f08eb
imojaek <imojaek@gmail.com>
2024-03-05 11:41:21 +0900

    PawnTest 상수명의 상수화
    colorBlack, colorWhite 이던 상수의 이름을 중복되는 의미의 color를 제거하고 대문자 BLACK, WHITE로 변경했습니다.

--- app/src/test/java/chess/PawnTest.java
+++ app/src/test/java/chess/PawnTest.java
@@ -2,21 +2,29 @@ package chess;
 
 import org.junit.jupiter.api.*;
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PawnTest {
 
     @Test
     @DisplayName("폰이 정상적으로 생성되는지 확인한다.")
     public void create() {
-        final String colorWhite = "white";
-        final String colorBlack = "black";
-        verifyPawnColor(colorWhite);
-        verifyPawnColor(colorBlack);
+        final String WHITE = "white";
+        final String BLACK = "black";
+        verifyPawnColor(WHITE);
+        verifyPawnColor(BLACK);
     }
 
     @DisplayName("폰의 color가 잘 들어가는지 확인한다.")
     private void verifyPawnColor(final String color) {
         Pawn pawnTest = new Pawn(color);
         assertThat(pawnTest.getColor()).isEqualTo(color);
+    }
+
+    @Test
+    @DisplayName("기본생성자로 생성되는 Pawn이 white 색상을 갖는지 확인한다.")
+    public void create_기본생성자() throws Exception {
+        Pawn pawn = new Pawn();
+        assertEquals("white", pawn.getColor());
     }
 }
