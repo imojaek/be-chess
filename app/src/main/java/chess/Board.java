4a6904805556db0840ac19ccb51c9cae534e5dd6
imojaek <imojaek@gmail.com>
2024-03-05 20:25:44 +0900

    Board를 초기화하는 initialize 작성
    먼저 add 메소드에 x,y좌표를 매개로 추가했고, 이를 통해 기물들을 board의 제 위치에 배치합니다.
    
    그리고 기물의 색깔로 구분해서 blackPieces, whitePieces 리스트에 넣어둡니다.
    
    이후 기물이 추가되면 initPawn 아래에 추가할 예정입니다.

--- app/src/main/java/chess/Board.java
+++ app/src/main/java/chess/Board.java
@@ -6,19 +6,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Board {
-    private Pawn[][] board; // -> Piece[][]
+    private Pawn[][] board = new Pawn[8][8]; // -> Piece[][]
     private static final int BOARD_X_MAX = 7;
     private static final int BOARD_Y_MAX = 7;
-    private List<Pawn> pawnList;
-
-    public Board() {
-        this.board = new Pawn[8][8];
-        pawnList = new ArrayList<>();
-    }
+    private List<Pawn> pawnList = new ArrayList<>();
+    private List<Pawn> blackPieces = new ArrayList<>();
+    private List<Pawn> whitePieces = new ArrayList<>();
 
     public void add(Pawn pawn, int xpos, int ypos) {
         board[xpos][ypos] = pawn;
         pawnList.add(pawn);
+        classifyPawn(pawn);
+    }
+
+    private void classifyPawn(Pawn pawn) {
+        if (pawn.getColor().equals(Pawn.BLACK_COLOR))
+            blackPieces.add(pawn);
+        else if (pawn.getColor().equals(Pawn.WHITE_COLOR))
+            whitePieces.add(pawn);
+    }
+
+    public void initialize() {
+        initPawn();
+    }
+
+    private void initPawn() {
+        for (int i = 0; i <= BOARD_X_MAX; i++) {
+            add(new Pawn(Pawn.WHITE_COLOR), i, BOARD_Y_MAX - 1);
+            add(new Pawn(Pawn.BLACK_COLOR), i, 0 + 1);
+        }
     }
 
     public int size() {
