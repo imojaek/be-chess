626d58ba69d284ef0f8cb28c3df39a025027e363
imojaek <imojaek@gmail.com>
2024-03-05 10:32:00 +0900

    검은 폰 테스트 추가
    Pawn 생성자로 "black" 을 전달해서 검은색 폰인 pawnBlack을 만들었습니다.
    그리고 pawnBlack의 color가 "black"인지 확인합니다.

--- app/src/test/java/chess/PawnTest.java
+++ app/src/test/java/chess/PawnTest.java
@@ -9,6 +9,8 @@ public class PawnTest {
     @DisplayName("흰색 폰이 생성되어야 한다")
     public void create() {
         Pawn pawn = new Pawn("white");
+        Pawn pawnBlack = new Pawn("black");
         assertThat(pawn.getColor()).isEqualTo("white");
+        assertThat(pawnBlack.getColor()).isEqualTo("black");
     }
 }
