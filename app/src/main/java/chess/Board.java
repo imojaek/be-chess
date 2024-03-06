cbff8d7b682cbbf7a06379e409a9784b2aca0f12
imojaek <imojaek@gmail.com>
2024-03-06 20:05:35 +0900

    pos추가

--- app/src/main/java/chess/Board.java
+++ app/src/main/java/chess/Board.java
@@ -6,18 +6,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Board {
+    private Pawn[][] board; // -> Piece[][]
+    private static final int BOARD_X_MAX = 7;
+    private static final int BOARD_Y_MAX = 7;
     private List<Pawn> pawnList;
 
     public Board() {
+        this.board = new Pawn[8][8];
         pawnList = new ArrayList<>();
     }
 
-    public void add(Pawn pawn) {
+    public void add(Pawn pawn, int xpos, int ypos) {
+        board[xpos][ypos] = pawn;
         pawnList.add(pawn);
     }
 
     public int size() {
-        return pawnList.size();
+        int boardSize = 0;
+        for (int x = 0; x <= BOARD_X_MAX; x++) {
+            for (int y = 0; y < BOARD_Y_MAX; y++) {
+                if (board[x][y] != null)
+                    boardSize++;
+            }
+        }
+        return boardSize;
     }
 
     public Pawn findPawn(int index) {
