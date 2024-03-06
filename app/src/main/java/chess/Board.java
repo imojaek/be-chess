27a665dc115439ef78b92fa33bcff4148bf6f68f
imojaek <imojaek@gmail.com>
2024-03-06 13:29:16 +0900

    수정 : 요구사항에서 벗어나는 코드 제거
    기존에 8*8 배열의 board를 선언하고, 빈자리를 찾아 폰을 추가하는 메소드를
    작성했었습니다
    
    이를 제거하고 요구사항의 내용과 테스트코드를 처리할 수 있을만큼의 코드만
    남기도록 했습니다.

--- app/src/main/java/chess/Board.java
+++ app/src/main/java/chess/Board.java
@@ -6,47 +6,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Board {
-    private Pawn[][] board;
-    private int xpos = 0;
-    private int ypos = 0;
-    private static final int BOARD_X_MAX = 7;
-    private static final int BOARD_Y_MAX = 7;
     private List<Pawn> pawnList;
 
     public Board() {
-        this.board = new Pawn[8][8];
         pawnList = new ArrayList<>();
     }
 
     public void add(Pawn pawn) {
-        if (findFreeBoard()) {
-            board[xpos][ypos] = pawn;
-            pawnList.add(pawn);
-        }
-    }
-
-    private boolean findFreeBoard() {
-        while (board[xpos][ypos] != null) {
-            if (xpos == BOARD_X_MAX) {
-                xpos = 0;
-                ypos++;
-            }
-            if (ypos == BOARD_Y_MAX + 1)
-                return false;
-            xpos++;
-        }
-        return true;
+        pawnList.add(pawn);
     }
 
     public int size() {
-        int boardSize = 0;
-        for (int x = 0; x <= BOARD_X_MAX; x++) {
-            for (int y = 0; y < BOARD_Y_MAX; y++) {
-                if (board[x][y] != null)
-                    boardSize++;
-            }
-        }
-        return boardSize;
+        return pawnList.size();
     }
 
     public Pawn findPawn(int index) {
