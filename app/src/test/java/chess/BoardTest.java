d896610a7acf155df3ac9b8a510ddd69eb8101d0
imojaek <imojaek@gmail.com>
2024-03-06 19:27:07 +0900

    board 타입 변경으로 인한 테스트코드 재작성
    board가 Pawn[][] 에서 HashMap<String, Pawn> 으로 변경되며  메소드들에도 변경점이 있었고,
    
    이를 반영해서 테스트코드를 수정했습니다.

--- app/src/test/java/chess/BoardTest.java
+++ app/src/test/java/chess/BoardTest.java
@@ -3,7 +3,6 @@ package chess;
 import org.junit.jupiter.api.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import chess.pieces.Pawn;
 
@@ -20,14 +19,14 @@ public class BoardTest {
     @DisplayName("보드에 폰을 추가하고 확인할 수 있어야 한다.")
     public void create() throws Exception {
         Pawn white = new Pawn(Pawn.WHITE_COLOR);
-        board.add(white, 0, 0);
-        assertEquals(1, board.size());
-        assertEquals(white, board.findPawn(0));
+        board.add(white, "B1");
+        assertThat(board.size()).isEqualTo(1);
+        assertThat(board.findPawn(0)).isEqualTo(white);
 
         Pawn black = new Pawn(Pawn.BLACK_COLOR);
-        board.add(black, 1, 0);
-        assertEquals(2, board.size());
-        assertEquals(black, board.findPawn(1));
+        board.add(black, "G1");
+        assertThat(board.size()).isEqualTo(2);
+        assertThat(board.findPawn(1)).isEqualTo(black);
     }
 
     @Test
@@ -36,23 +35,23 @@ public class BoardTest {
         Board board = new Board();
         board.initialize();
         assertThat(board.size()).isEqualTo(16); // Feature3 현재, 흰색 검은색 폰이 각각 8개씩 생성되어 배치된다.
-        assertEquals("♙♙♙♙♙♙♙♙", board.getWhitePawnsResult());
-        assertEquals("♟♟♟♟♟♟♟♟", board.getBlackPawnsResult());
+        assertThat(board.getWhitePawnsResult()).isEqualTo("♙♙♙♙♙♙♙♙");
+        assertThat(board.getBlackPawnsResult()).isEqualTo("♟♟♟♟♟♟♟♟");
     }
 
     @Test
     @DisplayName("색상에 따라 구분된 리스트에 기물들이 들어가야 한다.")
-    void classfyPiece() {
-        board.add(new Pawn(Pawn.BLACK_COLOR), 0, 0);
-        board.add(new Pawn(Pawn.BLACK_COLOR), 0, 1);
-        board.add(new Pawn(Pawn.WHITE_COLOR), 0, 2);
+    void classifyPiece() {
+        board.add(new Pawn(Pawn.BLACK_COLOR), "A1");
+        board.add(new Pawn(Pawn.BLACK_COLOR), "A2");
+        board.add(new Pawn(Pawn.WHITE_COLOR), "A3");
         assertThat(board.getBlackPieces().size()).isEqualTo(2);
         assertThat(board.getWhitePieces().size()).isEqualTo(1);
     }
 
     @Test
-    @DisplayName("현재 보드의 상태가 정상적으로 출력되어야 한다.")
-    void print() {
+    @DisplayName("초기화된 보드가 정상적으로 출력되어야 한다.")
+    void print_initialize() {
         board.initialize();
         System.out.println(board.print());
     }
